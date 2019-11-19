cdf7647ff90ee49639d6095a79edf9a65b0c90cb
Saara Koskipaa <saara.koskipaa@iki.fi>
2019-11-19 17:40:56 +0200

    Modified ui

--- src/main/java/ui/CommandLineUI.java
+++ src/main/java/ui/CommandLineUI.java
@@ -1,23 +1,27 @@
-
 package ui;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CommandLineUI {
+
     private Scanner reader;
+    private ArrayList<String> recommendations;
 
     public CommandLineUI(Scanner reader) {
         this.reader = reader;
+        this.recommendations = new ArrayList<>();
     }
 
     public void start() {
-        
+
         System.out.println("Welcome!");
-        System.out.println("1 --- List all recommended books");
-        System.out.println("2 --- Add a new book");
-        System.out.println("q --- Quit");
+        
 
         while (true) {
+            System.out.println("\n1 --- List all recommended books");
+            System.out.println("2 --- Add a new book");
+            System.out.println("q --- Quit");
             System.out.println("Select 1, 2 or q");
             String choice = reader.nextLine();
 
@@ -35,17 +39,35 @@ public class CommandLineUI {
 
         }
 
-        System.out.println("Thanks for using Recommendations! Have a nice day!");
-    }
-   
-    
-    private static void listBooks() {
-        System.out.println("Järjestelmässä ei ole vielä kirjoja...");
+        System.out.println("\nThanks for using Recommendations! Have a nice day!");
     }
 
-    private static void addBook() {
-        System.out.println("Not supported yet...");
+    private void listBooks() {
+        System.out.println("\nRecommendations:");
+        if (recommendations.isEmpty()) {
+            System.out.println("No recommendations yet. Be the first one to contribute!");
+        }
+        
+        recommendations.forEach((s) -> {
+            System.out.println(s);
+        });
+
     }
-    
-    
+
+    private void addBook() {
+        System.out.println("\nAdd a new Book");
+        System.out.print("Title: ");
+        String title = reader.nextLine();
+
+        System.out.print("Author: ");
+        String author = reader.nextLine();
+
+        System.out.println("ISBN: ");
+        String isbn = reader.nextLine();
+
+        recommendations.add(title);
+        System.out.println("A new book recommendation was added successfully!");
+
+    }
+
 }
