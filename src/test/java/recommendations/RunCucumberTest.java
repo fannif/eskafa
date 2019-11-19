1d65e0f58cfa807ecf411d4b92f3930853f983a3
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2019-11-19 12:33:54 +0200

    Update RunCucumberTest.java

---
+++ src/test/java/recommendations/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package recommendations;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = "pretty", 
+    features = "src/test/resources/recommendations", 
+    snippets = SnippetType.CAMELCASE 
+)
+
+public class RunCucumberTest {}
