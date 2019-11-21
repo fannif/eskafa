c960c0d21db31c18eda61b5bf2e49efc04744037
outisa <savolainen.outi@gmail.com>
2019-11-21 21:19:01 +0200

    remove commented code in testfile

--- src/test/java/recommendations/RecommendationsTest.java
+++ src/test/java/recommendations/RecommendationsTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package recommendations;
 
 import java.io.IOException;
@@ -112,10 +107,7 @@ public class RecommendationsTest {
         service.remove(title, lukija);
         
         assertEquals(3, service.listBooks().size());
-        //assertEquals(, readerDaoStub.findOne("Beyond Fear"));        
-        
     }
     
-  
 
 }
