1a96d92e8d009eb68b3b3f54760b623e2782633a
outisa <savolainen.outi@gmail.com>
2019-11-26 18:11:24 +0200

    odify fakeDaos

--- src/test/java/recommendations/FakeLinkDao.java
+++ src/test/java/recommendations/FakeLinkDao.java
@@ -2,35 +2,64 @@
 package recommendations;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 import recommendations.dao.ReaderDao;
 import recommendations.domain.Link;
 
 public class FakeLinkDao implements ReaderDao<Link, String> {
+    List<Link> tips = createTips();
 
     @Override
     public Link findOne(String title) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Link tip = findWanted(title);
+        return tip;
     }
 
     @Override
     public List<Link> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return tips;
     }
 
     @Override
     public boolean save(Link tip) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        tips.add(tip);
+        return true;
     }
 
     @Override
     public void delete(String title) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Link linkToDelete = findWanted(title);
+        if (linkToDelete != null) {
+            tips.remove(linkToDelete);
+        }
     }
 
     @Override
     public boolean edit(Link tip) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private Link findWanted(String title) {
+        Link wanted = null;
+        for (int i = 0; i < tips.size(); i++) {
+            if (tips.get(i).getTitle().equals(title)) {
+                wanted = tips.get(i);
+            }
+        }
+        return wanted;
+    }
+
+    private ArrayList<Link> createTips() {
+        ArrayList<Link> linkTips = new ArrayList<>();
+        ArrayList<String> tags = new ArrayList<>();
+        ArrayList<String> tags2 = new ArrayList<>();
+        tags.add("news");
+        ArrayList<String> courses = new ArrayList<>();
+        ArrayList<String> courses2 = new ArrayList<>();
+        courses2.add("Ohjelmistotuotanto");
+        linkTips.add(new Link(1, "Kaleva", "www.kaleva.fi", "Website", "", tags, courses, "news"));
+        linkTips.add(new Link(2, "", "", "Link", "https://ohjelmistotuotanto-hy.github.io/", tags2, courses2, ""));
+        return linkTips;
     }
     
 }
