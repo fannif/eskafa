444fed56d7765a6a6bdaa186ebe0268c96d72031
Fanni Franssila <fanni.franssila@gmail.com>
2019-11-26 14:11:31 +0200

    Added abstract methods, but no function yet

--- src/main/java/recommendations/dao/TagDao.java
+++ src/main/java/recommendations/dao/TagDao.java
@@ -1,6 +1,36 @@
 
 package recommendations.dao;
 
-public class TagDao {
+import java.sql.SQLException;
+import java.util.List;
+import recommendations.domain.Tag;
+
+public class TagDao implements ReaderDao<Tag, String> {
+
+    @Override
+    public Tag findOne(String title) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Tag> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean save(Tag tip) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String title) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean edit(Tag tip) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
