e6c7f03d007beee7d0ae9917b42787416436b7a7
Fanni Franssila <fanni.franssila@gmail.com>
2019-11-26 14:59:41 +0200

    Implemented findAll

--- src/main/java/recommendations/dao/CourseDao.java
+++ src/main/java/recommendations/dao/CourseDao.java
@@ -1,7 +1,62 @@
 
 package recommendations.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import recommendations.domain.Course;
 
-public class CourseDao {
+
+public class CourseDao implements ReaderDao<Course, String> {
+
+    private Database database;
+    
+    public CourseDao(Database database) {
+        this.database = database;
+    }
+    
+    @Override
+    public Course findOne(String title) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Course> findAll() throws SQLException {
+        List<Course> courses;
+        try (Connection connection = database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM Course");
+            courses = new ArrayList<>();
+            while (results.next()) {
+                int id = results.getInt("id");
+                String name = results.getString("name");
+
+                Course course = new Course(id, name);
+                courses.add(course);
+            }
+            connection.close();
+        }
+
+        return courses;
+    }
+    
+
+    @Override
+    public boolean save(Course tip) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String title) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean edit(Course tip) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/main/java/recommendations/dao/TagDao.java
+++ src/main/java/recommendations/dao/TagDao.java
@@ -1,12 +1,22 @@
 
 package recommendations.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import recommendations.domain.Tag;
 
 public class TagDao implements ReaderDao<Tag, String> {
 
+    private Database database;
+
+    public TagDao(Database database) {
+        this.database = database;
+    }
+    
     @Override
     public Tag findOne(String title) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -14,7 +24,22 @@ public class TagDao implements ReaderDao<Tag, String> {
 
     @Override
     public List<Tag> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Tag> tags;
+        try (Connection connection = database.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM Tag");
+            tags = new ArrayList<>();
+            while (results.next()) {
+                int id = results.getInt("id");
+                String name = results.getString("name");
+
+                Tag tag = new Tag(id, name);
+                tags.add(tag);
+            }
+            connection.close();
+        }
+
+        return tags;
     }
 
     @Override
