bda0534e6028911219e4265762d07420f303087b
outisa <savolainen.outi@gmail.com>
2019-12-07 12:34:13 +0200

    Add more colours to help user to know when input needed

--- src/main/java/recommendations/services/LinkService.java
+++ src/main/java/recommendations/services/LinkService.java
@@ -2,7 +2,6 @@ package recommendations.services;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import recommendations.domain.Book;
 import recommendations.domain.Link;
 
 import java.io.IOException;
@@ -24,8 +23,9 @@ public class LinkService {
     private IO io;
     private ReaderDao linkDao;
     private final String red = "\u001B[91m";
+    private final String green = "\u001b[32;1m";    
     private final String original = "\u001B[0m";
-    
+    private final String cyan = "\u001b[36;1m";
     public LinkService(ReaderDao dao, IO io) {
         this.io = io;
         this.linkDao = dao;
@@ -81,7 +81,7 @@ public class LinkService {
                 return;
             }
             if (linkDao.findOne(input) == null) {
-                io.print("No such link found. Please check the spelling and try again: ");
+                io.print(cyan+"No such link found. Please check the spelling and try again: "+original);
                 io.print("To return back to main menu, enter q");
                 input = io.read();
             } else {
@@ -89,12 +89,12 @@ public class LinkService {
                 go = false;
             }
         }
-        io.print("The link has been successfully removed");
+        io.print(green +"The link has been successfully removed"+original);
     }
     
     public String askForDescription() {
         io.print("Metadata description not found!");
-        io.print("Enter description manually or press enter for no description:");
+        io.print(cyan+"Enter description manually or press enter for no description:"+original);
         String descr = io.read();
         return descr;
         
@@ -123,20 +123,20 @@ public class LinkService {
                 return;
             }
             if (link == null) {
-                io.print("No such link found. Please check the spelling and try again: ");
+                io.print(cyan+"No such link found. Please check the spelling and try again: "+original);
                 io.print("To return back to main menu, enter q");
                 input = io.read();
             } else {
                 io.print("Link found: \n\t" + link);
                 System.out.println("Please update a field or fields. Press enter to skip the field.");
-                io.print("Title: ");
+                io.print(cyan+"Title: "+original);
                 String title = io.read();
-                io.print("Type: ");
+                io.print(cyan+"Type: "+original);
                 String type = io.read();
                 ArrayList<Tag> tags = new ArrayList();
                 ArrayList<Course> courses = new ArrayList();
-                io.print("Add zero or more tags. Enter tags one at a time. Press 'enter'"
-                        + "to continue: ");
+                io.print(cyan+"Add zero or more tags. Enter tags one at a time. Press 'enter'"
+                        + "to continue: "+original);
                 
                 while (true) {
                     String tag = io.read();
@@ -146,8 +146,8 @@ public class LinkService {
                     Tag newTag = new Tag(0, tag);
                     tags.add(newTag);
                 }
-                io.print("Add zero or more related courses. Enter courses one at a time. Press 'enter'"
-                        + "to continue: ");
+                io.print(cyan+"Add zero or more related courses. Enter courses one at a time. Press 'enter'"
+                        + "to continue: "+original);
                 
                 while (true) {
                     String course = io.read();
@@ -157,14 +157,14 @@ public class LinkService {
                     Course newCourse = new Course(0, course);
                     courses.add(newCourse);
                 }
-                io.print("Add a comment: ");
+                io.print(cyan+"Add a comment: "+original);
                 String comment = io.read();
                 
                 Link updated = updateLinkInformation(link, title, type, tags, courses, comment);
                 if (linkDao.edit(updated)) {
-                    io.print("The link information has been successfully updated.");
+                    io.print(green+"The link information has been successfully updated."+original);
                 } else {
-                    System.out.println("The link you are trying to add is already in the database.");
+                    System.out.println(red+"The link you are trying to add is already in the database."+original);
                 }
                 go = false;
             }
